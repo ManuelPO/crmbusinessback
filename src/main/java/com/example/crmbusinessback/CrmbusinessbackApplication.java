4ccab6a671f1b6bacfda63fe3f40ddf47cac38e5
ManuelPrietoOrtiz <manuel.prietoor@gmail.com>
2022-12-01 10:15:45 +0100

    Initial commit

---
+++ src/main/java/com/example/crmbusinessback/CrmbusinessbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.crmbusinessback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmbusinessbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmbusinessbackApplication.class, args);
+	}
+
+}
